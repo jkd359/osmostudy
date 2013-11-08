b0549b3e69312f9a421749a7558bf232a64c5944
Joel Dwyer <joel.dwyer@nodramas.com.au>
2013-11-08 12:23:41 +1100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Osmostudy::Application.config.secret_key_base = '1a80b7720a13bcbc6d2ec4322872307c2240f290991dfc6a8d641c9942af9f6919f71042bc91dd86b8a35930e88b46a4e65c0b9540cffdfedbc582a194d9dc12'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Osmostudy::Application.config.session_store :cookie_store, key: '_osmostudy_session'
